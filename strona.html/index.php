a1c7543c155de8d6d461e2d7730b6a07b5452ca3
FabianAdamiak <fabianadamiak@gmail.com>
2022-12-07 11:57:34 +0100

    koncowe zadanie zrobione

--- strona.html/index.php
+++ strona.html/index.php
@@ -1,13 +1,13 @@
 <html>
     <head>
     <meta charset="UTF-8">
-    <title>OLX dla gorszych</title>
+    <title>sklep dla ubogich</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
     $db = new mysqli("localhost", "root", "", "shop");
-
+    
     if(isset($_POST["out"])){
         if($_POST["out"] == 1){
             setcookie("userID", NULL);
@@ -18,11 +18,13 @@
     if(!isset($_COOKIE["userID"])){
         if(isset($_POST["login"]) && isset($_POST["password"])){
             $sql = "SELECT `id` FROM `account` WHERE `login`='".$_POST["login"]."' AND `password`='".md5($_POST["password"])."';";
-
-            if($res = $db->query($sql))
+        }
+    }
+    
+            if($res = $db -> query ($user_add));
                 if(mysqli_num_rows($res) != 0){
                     $logedID = $res->fetch_assoc()["id"];
                     setcookie("userID", $logedID);
                     header('Location: index.php');
-                    {
-                        </html>
+                }
+    
